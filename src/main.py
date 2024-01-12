9ce3c28ef0e96fac93c55867caf63592cb47e70c
miraz <mohammad.miraz@northsouth.edu>
2024-01-13 02:27:30 +0600

    Update with all boilerplate data

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from starlette.middleware.cors import CORSMiddleware
+from apis.modulename import router_name
+from apis.health_check_module import health_check_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router_name)
+app.include_router(health_check_router)
